f44a6edf15d70247500d8e10d358f065d7e40c0b
Justin Cooper <jcooper@chicousd.org>
2022-07-26 13:17:00 -0700

    functioned

--- sql/escape-aeries-users2.sql
+++ sql/escape-aeries-users2.sql
@@ -25,7 +25,7 @@ SELECT
    EmailWork LIKE '%@chico%'
    AND EmploymentStatusCode NOT IN ('R','T','D','X')
    AND SiteId IN ( 010,020,030,050,060,070,080,090,111,110,120,130,160,180,190,200,210,230,240,250,260,270,280,640,740 )
-   AND JobCategoryId NOT IN ('BUILDING','DRIVER')
+   AND JobCategoryId NOT IN ('BUILDING','DRIVER','INSTRUCT','NUTRI')
    -- PersonTypeId IN (1,2,4)
    -- AND
    -- AND BargUnitId IN ('CUTA','CUMA','CSEA','CHRT')
