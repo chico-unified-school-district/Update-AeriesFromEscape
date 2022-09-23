41ceec1a8ca0af47802b9ee9510cfaf42fa21ff3
Justin Cooper <jcooper@chicousd.org>
2022-09-23 11:09:46 -0700

    more robust ad sessions. fixed uid

--- sql/escape-aeries-users.sql
+++ sql/escape-aeries-users.sql
@@ -1,26 +1,37 @@
 SELECT
-   vwHREmploymentList.empID,
-   vwHREmploymentList.EmailWork,
-   vwHREmploymentList.NameLast,
-   vwHREmploymentList.NameFirst,
-   vwHREmploymentList.BargUnitId,
-   vwHREmploymentList.emailHome,
-   vwHREmploymentList.homeAddrCity AS CY,
-   vwHREmploymentList.homeAddrStateCode AS ST,
-   vwHREmploymentList.homeAddrStreet AS AD,
-   vwHREmploymentList.homeAddrZip AS ZC,
-   vwHREmploymentList.homePhone AS TL,
-   vwHREmploymentList.homePhoneMobile AS CP,
-   CASE
- WHEN vwHREmploymentList.BargUnitId = 'CSEA' THEN 'C'
- WHEN vwHREmploymentList.BargUnitId = 'CUTA' THEN 'T'
- WHEN vwHREmploymentList.BargUnitId = 'CUMA' THEN 'A'
-   ELSE NULL END AS U8 -- Aeries User defined field 8 - Tag Identification of Job Category
-  FROM vwHREmploymentList as HREvw
-   LEFT JOIN HREmployment as HRE ON HREmployment.EmpID = vwHREmploymentList.EmpID
+   empID AS empId
+   ,SiteId AS siteId
+   ,EmailWork AS emailW
+   ,NameLast AS ln
+   ,NameFirst AS fn
+   ,BargUnitId AS bargId
+   ,EmailHome AS emailH
+   -- ,HomeAddrCity AS city
+   -- ,HomeAddrStateCode AS state
+   -- ,HomeAddrStreet AS addr
+   -- ,HomeAddrZip AS zip
+   -- ,HomePhone AS phone
+   -- ,HomePhoneMobile AS cell
+   ,EmploymentTypeCode AS empType
+   ,EmploymentStatusCode AS empStatus
+,JobCategoryId
+,JobCategoryDescr
+,JobClassId
+   ,JobClassDescr AS jobDesc
+   ,PersonTypeId AS persId
+   ,DateTimeEdited AS dts
+  FROM vwHREmploymentList
   WHERE
-   HREmployment.PersonTypeId IN (1,2,4)
-   AND vwHREmploymentList.BargUnitId IN ('CUTA','CUMA','CSEA','CHRT')
-   AND HREmployment.EmploymentStatusCode IN ('A','I','L','W')
-   AND HREmployment.DateTimeEdited > DATEADD(day,-60,getdate())
-  ORDER BY vwHREmploymentList.empID DESC
+   EmailWork LIKE '%@chico%'
+   AND EmploymentStatusCode NOT IN ('R','T','D','X')
+   AND SiteId IN ( 010,020,030,050,060,070,080,090,111,110,120,130,160,180,190,200,210,230,240,250,260,270,280,640,740 )
+   AND JobCategoryId NOT IN ('BUILDING','DRIVER','INSTRUCT','NUTRI')
+   -- PersonTypeId IN (1,2,4)
+   -- AND
+   -- AND BargUnitId IN ('CUTA','CUMA','CSEA','CHRT')
+   AND BargUnitId NOT IN ('REST')
+   AND EmploymentStatusCode IN ('A','I','L','W')
+   -- AND DateTimeEdited > DATEADD(day,-60,getdate())
+  ORDER BY JobCategoryId;
+
+--   select distinct JobCategoryId from vwHREmploymentList order by JobCategoryId;
