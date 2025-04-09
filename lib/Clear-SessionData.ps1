a0a076157c391b3cc8c230f4cdabeedccfdadae4
Justin Cooper <jcooper@chicousd.org>
2025-04-09 07:55:15 -0700

    removed lib

--- lib/Clear-SessionData.ps1
+++
@@ -1,5 +0,0 @@
-function Clear-SessionData {
- Write-Verbose 'Clearing session and module data'
- Get-Module -name *tmp* | Remove-Module -Confirm:$false -Force
- Get-PSSession | Remove-PSSession -Confirm:$false
-}
