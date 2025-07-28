1ce4623172fff70b06ea8810b1ce479edd5eed9b
Justin Cooper <jcooper@chicousd.org>
2025-07-28 10:08:54 -0700

    removed datetimeedited from sql

--- sql/escape-aeries-users.sql
+++ sql/escape-aeries-users.sql
@@ -18,22 +18,22 @@ SELECT
   FROM vwHREmploymentList
   WHERE
     -- Regular employee accounts
-    ( EmailWork LIKE '%@chico%'
-    AND EmploymentStatusCode NOT IN ('R','T','D','X')
-    AND SiteId IN ( 010,020,030,050,060,070,080,090,111,110,120,130,160,180,190,200,210,230,240,250,260,270,280,640,740 )
-    AND JobCategoryId NOT IN ('BUILDING','DRIVER','INSTRUCT','NUTRI')
-    -- AND PersonTypeId IN (1,2,4)
-    -- AND BargUnitId IN ('CUTA','CUMA','CSEA','CHRT')
-    AND BargUnitId NOT IN ('REST')
-    AND EmploymentStatusCode IN ('A','I','L','W')
-    AND DateTimeEdited > DATEADD(day,-5,getdate())
+    (
+      EmailWork LIKE '%@chico%'
+      AND EmploymentStatusCode NOT IN ('R','T','D','X')
+      AND SiteId IN ( 010,020,030,050,060,070,080,090,111,110,120,130,160,180,190,200,210,230,240,250,260,270,280,640,740 )
+      AND JobCategoryId NOT IN ('BUILDING','DRIVER','INSTRUCT','NUTRI')
+      -- AND PersonTypeId IN (1,2,4)
+      -- AND BargUnitId IN ('CUTA','CUMA','CSEA','CHRT')
+      AND BargUnitId NOT IN ('REST')
+      AND EmploymentStatusCode IN ('A','I','L','W')
     )
     -- Student worker accounts
     OR (
       PersonTypeId = 6
-      AND DateTimeEdited > DATEADD(day,-5,getdate())
       AND EmploymentStatusCode IN ('A')
       )
-  ORDER BY JobCategoryId;
+    -- AND DateTimeEdited > DATEADD(day,-14,getdate())
+  ORDER BY EmpId;
 
 --   select distinct JobCategoryId from vwHREmploymentList order by JobCategoryId;
